ddc5e5372a7e612d1b13eb7b87748fa46d8b9e41
ezevin <ehzevin@gmail.com>
2019-05-06 16:25:10 -0400

    slidy tily

--- frontend/index.js
+++ frontend/index.js
@@ -33,23 +33,24 @@ const gameBoard = document.getElementById("game-board")
 //              Swapping Tiles-Website            //
 //*************************************************//
 function swapTiles(cell1,cell2) {
-  var temp = document.getElementById(cell1).className;
+  let temp = document.getElementById(cell1).className;
   document.getElementById(cell1).className = document.getElementById(cell2).className;
   document.getElementById(cell2).className = temp;
 }
 
 function shuffle() {
 //Use nested loops to access each cell of the 3x3 grid
-for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
-   for (var column=1;column<=3;column++) { //For each column in this row
+for (let row=1;row<=3;row++) { //For each row of the 3x3 grid
+   for (let column=1;column<=3;column++) { //For each column in this row
 
-    var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
-    var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
+    let row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
+    let column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
 
     swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
   }
 }
 }
+
 
 function clickTile(row,column) {
   var cell = document.getElementById("cell"+row+column);
@@ -94,16 +95,34 @@ function clickTile(row,column) {
 ////////////////Create A Keydown Event///////////
 document.addEventListener("keydown", function(e){
   // console.log("click")
-  const movingTile = gameBoard.firstElementChild.lastElementChild
-  if(event.keyCode == 37) {
+  let mover = document.querySelector(".tile9")
+  const row = mover.id.split("")[4]
+  const column = mover.id.split("")[5]
+
+  if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 ||e.keyCode == 40)
+  // debugger
+  if(event.keyCode == 37  ) {
+      let c = parseInt(column) - 1
+      // debugger
+      clickTile(row, c)
        console.log('Left was pressed');
   } else if(event.keyCode == 39) {
+    let c = parseInt(column) + 1
+    // debugger
+    clickTile(row, c)
         console.log('Right was pressed');
   } else if(event.keyCode == 38) {
+    let r = parseInt(row) - 1
+    // debugger
+    clickTile(r, column)
         console.log('Up was pressed');
   }else if(event.keyCode == 40) {
+    let r = parseInt(row) + 1
+    // debugg er
+    clickTile(r, column)
         console.log('Down was pressed');
   }else {
+    clickTile(row, column)
     console.log("nope!")
   }
   // debugger
